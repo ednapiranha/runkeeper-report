96951e6f1630c84f7ecf2de6f87dd65258182beb
Jen Fong-Adwent <jen@ednapiranha.com>
2013-07-07 09:44:33 -0400

    map details style updates

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -39,7 +39,8 @@ define(['jquery', 'utils', 'settings', 'base/activity', 'nunjucks', 'templates']
         break;
 
       case 'close':
-        body.find('#detail').addClass('hidden');
+        body.find('#detail').addClass('hidden')
+        body.find('#map-canvas').empty();
         break;
     }
   });
